56b7693a17ccc71f9fd8bd7cf52fa0c045edb4c8
Jevgenija Beridze <jevgenija@mbpr10.home>
2017-06-18 21:11:31 +0200

    Initial Commit

---
+++ MedJe/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MedJe
+//
+//  Created by Jevgenija Beridze on 18/06/2017.
+//  Copyright © 2017 Jevgenija Beridze. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
