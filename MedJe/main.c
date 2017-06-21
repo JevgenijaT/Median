749201594cc70c5773498f97bdd3a9f73b0f4560
JevgenijaT <musition2006@yahoo.com>
2017-06-21 12:03:12 +0200

    Update main.c

--- MedJe/main.c
+++ MedJe/main.c
@@ -1,15 +1,41 @@
-//
-//  main.c
-//  MedJe
-//
-//  Created by Jevgenija Beridze on 18/06/2017.
-//  Copyright © 2017 Jevgenija Beridze. All rights reserved.
-//
+#define N 10
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+main( )
+{
+    int i,j,n;
+    float med,arr[N],temp;
+    printf( "Please enter the number of items:\n");
+    scanf("%d", &n);
+    /*Reading item into array a*/
+    
+    printf("input %d values\n",n);
+    for (i=1;i<=n;i++)
+        scanf("%f", &arr[i]);
+    
+    /* Sort begins */
+    for (i = 1 ; i <= n-1 ; i++)
+    {
+        for (j = 1 ; j <= n-i ; j++)
+        {
+            if (arr[j] <= arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+            else
+                continue ;
+        }
+    } /* sort ends */
+    /* calculation of median  */
+    if ( n % 2 == 0)
+        med = (arr[n/2] + arr[n/2+1])/2.0 ;
+    else
+        med = arr[n/2 + 1];
+    
+    /*  Printing values */
+    for (i = 1 ; i <= n ; i++)
+        printf("%f  ", arr[i]);
+    printf("\n\nMedian value is: %f\n", med);
+    
 }
